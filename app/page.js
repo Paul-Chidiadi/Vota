cc2595b4b4a2395067a246c26bb27630af4a6378
Paul Chidiadi <paulchidiadi@gmail.com>
2023-11-07 17:37:40 +0100

    Deliver Top and Hero Section of Home page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,47 @@
+'use client';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  return <main></main>;
+  const router = useRouter();
+  return (
+    <main>
+      <section className="home-page-top">
+        <Link className="app-name links" href="/">
+          VOTA<span className="material-symbols-outlined">task_alt</span>
+        </Link>
+        <div>
+          <Link className="join links" href="/login">
+            Log in
+          </Link>
+          <button className="btn" onClick={() => router.push('/sign')}>
+            Sign Up
+          </button>
+        </div>
+      </section>
+
+      <section className="home-page-hero">
+        <div>
+          <h1>Making polling easy with VOTA</h1>
+          <p>
+            Forget the old way, You can have the best process. <br /> Right
+            here. Right now!
+          </p>
+          <Link className="btn links" href="/sign">
+            Get Started
+          </Link>
+        </div>
+        <div className="col-2">
+          <div class="image-container">
+            <span className="material-symbols-outlined image">task_alt</span>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div></div>
+      </section>
+    </main>
+  );
 }
