25ac37651fb13867f0e073a67d82f5b233de56f4
Paul Chidiadi <paulchidiadi@gmail.com>
2023-11-08 22:23:49 +0100

    Deliver organization and Elector layouts for signup page

--- app/signup/page.js
+++ app/signup/page.js
@@ -19,7 +19,11 @@ export default function Sign() {
     },
   ]);
   const [account, setAccount] = useState('');
+  const [selectAccountClassName, setSelectAccountClassName] = useState('inner-container selected');
+  const [orgClassName, setOrgClassName] = useState('org');
+  const [electClassName, setElectClassName] = useState('elect');
 
+  //HANDLE SELECTION FUNCTION
   function handleClick(id) {
     if (id === 1) {
       setAccount('Organization');
@@ -28,11 +32,33 @@ export default function Sign() {
     }
     setOptions((prev) => {
       return prev.map((item) => {
-        return item.id === id
-          ? { ...item, isSelected: true }
-          : { ...item, isSelected: false };
+        return item.id === id ? { ...item, isSelected: true } : { ...item, isSelected: false };
       });
     });
+  }
+
+  //HANDLE SWITCH FUNCTION
+  function handleSwitch() {
+    if (account === 'Organization') {
+      setAccount('Elector');
+      setOrgClassName('org');
+      setElectClassName('elect selected');
+    } else if (account === 'Elector') {
+      setAccount('Organization');
+      setElectClassName('elect');
+      setOrgClassName('org selected');
+    }
+  }
+
+  //HANDLE JOIN FUNCTION
+  function joinNow() {
+    if (account === 'Organization') {
+      setSelectAccountClassName('inner-container');
+      setOrgClassName('org selected');
+    } else if (account === 'Elector') {
+      setSelectAccountClassName('inner-container');
+      setElectClassName('elect selected');
+    }
   }
 
   const optionElements = options.map((option) => {
@@ -43,13 +69,7 @@ export default function Sign() {
         onClick={() => handleClick(option.id)}
       >
         <div className="circle"></div>
-        <Image
-          className="choose-img"
-          src={option.image}
-          alt="image info"
-          width={100}
-          height={70}
-        />
+        <Image className="choose-img" src={option.image} alt="image info" width={100} height={70} />
         <h4>{option.text}</h4>
       </div>
     );
@@ -64,17 +84,71 @@ export default function Sign() {
       </section>
 
       <section className="signup-page-body">
-        <div className="inner-container">
+        {/* SELECT ACCOUNT SECTION */}
+        <div className={selectAccountClassName}>
           <h1>Join as an organization or elector</h1>
           <div className="choose">
             {/* dynamically display options */}
             {optionElements}
           </div>
-          {account !== '' && <button className="btn">Create Account</button>}
+          {account !== '' && (
+            <button onClick={joinNow} className="btn">
+              Join as an {account}
+            </button>
+          )}
           <p>
             Already have an account?{' '}
             <Link className="links" href="/login">
               Login
+            </Link>
+          </p>
+        </div>
+
+        {/* ORGANIZATION SECTION */}
+        <div className={orgClassName}>
+          <h1>Joining as an {account}</h1>
+          <form className="forms">
+            <input type="name" placeholder="Organization name" />
+            <input type="email" placeholder="Work email address" />
+            <input type="password" placeholder="password" />
+            <input type="password" placeholder="confirm password" />
+            <button onClick={() => {}} className="btn">
+              Create my account
+            </button>
+          </form>
+          <p>
+            Already have an account?{' '}
+            <Link className="links" href="/login">
+              Login
+            </Link>
+          </p>
+          <p onClick={handleSwitch}>
+            <Link className="links" href="/signup">
+              Join as an Elector.
+            </Link>
+          </p>
+        </div>
+
+        {/* ELECTOR SECTION */}
+        <div className={electClassName}>
+          <h1>Joining as an {account}</h1>
+          <form className="forms">
+            <input type="email" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <input type="password" placeholder="confirm password" />
+            <button onClick={() => {}} className="btn">
+              Create my account
+            </button>
+          </form>
+          <p>
+            Already have an account?{' '}
+            <Link className="links" href="/login">
+              Login
+            </Link>
+          </p>
+          <p onClick={handleSwitch}>
+            <Link className="links" href="/signup">
+              Join as an Organization.
             </Link>
           </p>
         </div>
