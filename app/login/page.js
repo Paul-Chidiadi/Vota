189a258acce6f58a7252e6bfa6f7bdb745bbc4b3
Paul Chidiadi <paulchidiadi@gmail.com>
2023-11-09 19:04:47 +0100

    Created login page, delivered media query for signup and login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,4 +1,36 @@
+'use client';
+import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Login() {
-  return <main></main>;
+  const [electClassName, setElectClassName] = useState('elect selected');
+
+  return (
+    <main>
+      <section className="home-page-top">
+        <Link className="app-name links" href="/">
+          VOTA<span className="material-symbols-outlined">task_alt</span>
+        </Link>
+      </section>
+
+      <section className="signup-page-body">
+        <div className={electClassName}>
+          <h1>Login to Vota</h1>
+          <form className="forms">
+            <input type="email" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <button onClick={() => {}} className="btn">
+              Login
+            </button>
+          </form>
+          <p>
+            Already have an account?{' '}
+            <Link className="links" href="/signup">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
