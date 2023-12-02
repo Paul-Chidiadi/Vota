d639700a9a2526121db1937906d7e6da5d375563
Paul Chidiadi <paulchidiadi@gmail.com>
2023-12-02 21:10:33 +0100

    Made new event sections responsive

--- app/dashboard/organization/newevent/page.js
+++ app/dashboard/organization/newevent/page.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import '../../../../components/global/orgpage.css';
 import { useRouter } from 'next/navigation';
+import { v4 as uuidv4 } from 'uuid';
 
 const page = () => {
   const router = useRouter();
@@ -13,9 +14,9 @@ const page = () => {
     schedule: '',
     public: false,
     eventType: '',
-    positions: [],
+    positions: [{ id: uuidv4(), text: '' }],
     candidates: [],
-    pollQuestions: [],
+    pollQuestions: [{ id: uuidv4(), text: '' }],
   });
   const [slideNum, setSlideNum] = useState(1);
 
@@ -31,10 +32,10 @@ const page = () => {
       myElementRef.current.style.transform = `translateX(-0%)`;
       setSlideNum((prev) => prev - 1);
     } else if (slideNum === 3) {
-      myElementRef.current.style.transform = `translateX(-100%)`;
+      myElementRef.current.style.transform = `translateX(-106%)`;
       setSlideNum((prev) => prev - 1);
     } else if (slideNum === 4) {
-      myElementRef.current.style.transform = `translateX(-200%)`;
+      myElementRef.current.style.transform = `translateX(-212%)`;
       setSlideNum((prev) => prev - 1);
     }
   }
@@ -44,7 +45,7 @@ const page = () => {
     {
       id: 1,
       title: 'Poll',
-      text: 'Set up Poll where members can choose from different options.',
+      text: 'Set up a Poll where members can choose from different options.',
       image: '/images/Get-Organized.png',
       isSelected: false,
     },
@@ -57,6 +58,7 @@ const page = () => {
     },
   ]);
   const [account, setAccount] = useState('');
+  //Option elements
   const optionElements = options.map((option) => {
     return (
       <div
@@ -73,7 +75,12 @@ const page = () => {
   //HANDLE SELECTION FUNCTION
   function handleClick(id) {
     setEventDetails((prev) => {
-      return { ...prev, eventType: id === 1 ? 'Poll' : 'Election' };
+      return {
+        ...prev,
+        eventType: id === 1 ? 'Poll' : 'Election',
+        positions: id === 1 ? [{ id: uuidv4(), text: '' }] : prev.positions,
+        pollQuestions: id === 1 ? prev.pollQuestions : [{ id: uuidv4(), text: '' }],
+      };
     });
     setOptions((prev) => {
       return prev.map((item) => {
@@ -82,6 +89,7 @@ const page = () => {
     });
   }
 
+  console.log(eventDetails);
   return (
     <section className="org-main-page">
       <div className="new-event-section" id="myElement" ref={myElementRef}>
@@ -153,12 +161,9 @@ const page = () => {
         {/* set event type section */}
         <div className="flow set-type">
           <h1>Choose Event Type</h1>
-          <small>Choose which event type you are creating. Poll event or Election event proper</small>
+          <small>Choose which event type you are creating. Poll event or proper Election event.</small>
           <div className="inner-container selected">
-            <div className="choose">
-              {/* dynamically display options */}
-              {optionElements}
-            </div>
+            <div className="choose">{optionElements}</div>
           </div>
           <div className="actions">
             <button className="btnn" onClick={prevSlide}>
@@ -174,8 +179,179 @@ const page = () => {
             </button>
           </div>
         </div>
-        <div className="flow">three</div>
-        <div className="flow">four</div>
+
+        {/* set event proper section */}
+        <div className="flow">
+          {eventDetails.eventType === 'Election' ? (
+            //For election type
+            <div className="poll-questions">
+              <h3>
+                Set Election Positions{' '}
+                <i
+                  className="bx bx-plus"
+                  onClick={() => {
+                    setEventDetails((prev) => {
+                      return {
+                        ...prev,
+                        positions: [...prev.positions, { id: uuidv4(), text: '' }],
+                      };
+                    });
+                  }}
+                ></i>
+              </h3>
+              <div className="poll-list">
+                {eventDetails.positions.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      <input
+                        type="text"
+                        placeholder="Set Election Positions"
+                        onChange={(e) => {
+                          setEventDetails((prev) => {
+                            return {
+                              ...prev,
+                              positions: prev.positions.map((poll) => {
+                                return poll.id === item.id ? { ...poll, text: e.target.value } : poll;
+                              }),
+                            };
+                          });
+                        }}
+                      />
+                      <i
+                        className="bx bx-x"
+                        onClick={() => {
+                          setEventDetails((prev) => {
+                            return {
+                              ...prev,
+                              positions: prev.positions.filter((option) => {
+                                return option.id !== item.id;
+                              }),
+                            };
+                          });
+                        }}
+                      ></i>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="actions">
+                <button className="btnn" onClick={prevSlide}>
+                  {'<'} PREV
+                </button>
+                <button
+                  className="btnn"
+                  onClick={nextSlide}
+                  style={
+                    eventDetails.positions.length !== 0 && eventDetails.positions.every((item) => item.text.length > 0)
+                      ? {}
+                      : { color: 'var(--black-a30)' }
+                  }
+                  disabled={
+                    eventDetails.positions.length !== 0 && eventDetails.positions.every((item) => item.text.length > 0)
+                      ? false
+                      : true
+                  }
+                >
+                  NEXT {'>'}
+                </button>
+              </div>
+            </div>
+          ) : (
+            //For Poll type
+            <div className="poll-questions">
+              <h3>
+                Set Poll Questions{' '}
+                <i
+                  className="bx bx-plus"
+                  onClick={() => {
+                    setEventDetails((prev) => {
+                      return {
+                        ...prev,
+                        pollQuestions: [...prev.pollQuestions, { id: uuidv4(), text: '' }],
+                      };
+                    });
+                  }}
+                ></i>
+              </h3>
+              <div className="poll-list">
+                {eventDetails.pollQuestions.map((item) => {
+                  return (
+                    <div key={item.id}>
+                      <input
+                        type="text"
+                        placeholder="Set Poll Question"
+                        onChange={(e) => {
+                          setEventDetails((prev) => {
+                            return {
+                              ...prev,
+                              pollQuestions: prev.pollQuestions.map((poll) => {
+                                return poll.id === item.id ? { ...poll, text: e.target.value } : poll;
+                              }),
+                            };
+                          });
+                        }}
+                      />
+                      <i
+                        className="bx bx-x"
+                        onClick={() => {
+                          setEventDetails((prev) => {
+                            return {
+                              ...prev,
+                              pollQuestions: prev.pollQuestions.filter((option) => {
+                                return option.id !== item.id;
+                              }),
+                            };
+                          });
+                        }}
+                      ></i>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="actions">
+                <button className="btnn" onClick={prevSlide}>
+                  {'<'} PREV
+                </button>
+                <button
+                  className="btnn"
+                  onClick={nextSlide}
+                  style={
+                    eventDetails.pollQuestions.length !== 0 &&
+                    eventDetails.pollQuestions.every((item) => item.text.length > 0)
+                      ? {}
+                      : { color: 'var(--black-a30)' }
+                  }
+                  disabled={
+                    eventDetails.pollQuestions.length !== 0 &&
+                    eventDetails.pollQuestions.every((item) => item.text.length > 0)
+                      ? false
+                      : true
+                  }
+                >
+                  NEXT {'>'}
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* confirm section */}
+        <div className="flow confirm-section">
+          <div>
+            <h4>Good job setting up a new event!</h4>
+            <small>
+              When you click on submit, you can view this event and make changes to it in the future. Completed events
+              become history and can't be edited.
+            </small>
+          </div>
+          <span className="material-symbols-outlined image">task_alt</span>
+          <div className="actions">
+            <button className="btn" onClick={prevSlide}>
+              back
+            </button>
+            <button className="btn">submit</button>
+          </div>
+        </div>
       </div>
     </section>
   );
