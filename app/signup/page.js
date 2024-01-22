a1f43607370dbec9d160fd35f37f46774d48ebbe
Paul Chidiadi <paulchidiadi@gmail.com>
2024-01-22 02:27:35 +0100

    feat: modified elector signup section

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,75 +1,85 @@
-'use client';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState } from 'react';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Sign() {
   const [options, setOptions] = useState([
     {
       id: 1,
       text: "I'm an organization, managing a poll.",
-      image: '/images/Get-Organized.png',
+      image: "/images/Get-Organized.png",
       isSelected: false,
     },
     {
       id: 2,
       text: "I'm an elector, participating in a poll.",
-      image: '/images/Get-Close.png',
+      image: "/images/Get-Close.png",
       isSelected: false,
     },
   ]);
-  const [account, setAccount] = useState('');
-  const [selectAccountClassName, setSelectAccountClassName] = useState('inner-container selected');
-  const [orgClassName, setOrgClassName] = useState('org');
-  const [electClassName, setElectClassName] = useState('elect');
+  const [account, setAccount] = useState("");
+  const [selectAccountClassName, setSelectAccountClassName] = useState(
+    "inner-container selected"
+  );
+  const [orgClassName, setOrgClassName] = useState("org");
+  const [electClassName, setElectClassName] = useState("elect");
 
   //HANDLE SELECTION FUNCTION
   function handleClick(id) {
     if (id === 1) {
-      setAccount('Organization');
+      setAccount("Organization");
     } else if (id === 2) {
-      setAccount('Elector');
+      setAccount("Elector");
     }
     setOptions((prev) => {
       return prev.map((item) => {
-        return item.id === id ? { ...item, isSelected: true } : { ...item, isSelected: false };
+        return item.id === id
+          ? { ...item, isSelected: true }
+          : { ...item, isSelected: false };
       });
     });
   }
 
   //HANDLE SWITCH FUNCTION
   function handleSwitch() {
-    if (account === 'Organization') {
-      setAccount('Elector');
-      setOrgClassName('org');
-      setElectClassName('elect selected');
-    } else if (account === 'Elector') {
-      setAccount('Organization');
-      setElectClassName('elect');
-      setOrgClassName('org selected');
+    if (account === "Organization") {
+      setAccount("Elector");
+      setOrgClassName("org");
+      setElectClassName("elect selected");
+    } else if (account === "Elector") {
+      setAccount("Organization");
+      setElectClassName("elect");
+      setOrgClassName("org selected");
     }
   }
 
   //HANDLE JOIN FUNCTION
   function joinNow() {
-    if (account === 'Organization') {
-      setSelectAccountClassName('inner-container');
-      setOrgClassName('org selected');
-    } else if (account === 'Elector') {
-      setSelectAccountClassName('inner-container');
-      setElectClassName('elect selected');
+    if (account === "Organization") {
+      setSelectAccountClassName("inner-container");
+      setOrgClassName("org selected");
+    } else if (account === "Elector") {
+      setSelectAccountClassName("inner-container");
+      setElectClassName("elect selected");
     }
   }
 
   const optionElements = options.map((option) => {
     return (
       <div
-        className={option.isSelected ? 'col selected' : 'col'}
+        className={option.isSelected ? "col selected" : "col"}
         key={option.id}
         onClick={() => handleClick(option.id)}
       >
         <div className="circle"></div>
-        <Image className="choose-img" src={option.image} alt="image info" width={100} height={70} />
+        <Image
+          className="choose-img"
+          src={option.image}
+          alt="image info"
+          width={100}
+          height={70}
+        />
         <h4>{option.text}</h4>
       </div>
     );
@@ -91,13 +101,13 @@ export default function Sign() {
             {/* dynamically display options */}
             {optionElements}
           </div>
-          {account !== '' && (
+          {account !== "" && (
             <button onClick={joinNow} className="btn">
               Join as an {account}
             </button>
           )}
           <p>
-            Already have an account?{' '}
+            Already have an account?{" "}
             <Link className="links" href="/login">
               Login
             </Link>
@@ -117,7 +127,7 @@ export default function Sign() {
             </button>
           </form>
           <p>
-            Already have an account?{' '}
+            Already have an account?{" "}
             <Link className="links" href="/login">
               Login
             </Link>
@@ -133,6 +143,7 @@ export default function Sign() {
         <div className={electClassName}>
           <h1>Joining as an {account}</h1>
           <form className="forms">
+            <input type="name" placeholder="Full Name" />
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
             <input type="password" placeholder="Confirm Password" />
@@ -141,7 +152,7 @@ export default function Sign() {
             </button>
           </form>
           <p>
-            Already have an account?{' '}
+            Already have an account?{" "}
             <Link className="links" href="/login">
               Login
             </Link>
