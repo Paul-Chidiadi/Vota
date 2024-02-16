98d5893cb52450698199808576dff032e970ba8f
Paul Chidiadi <paulchidiadi@gmail.com>
2024-02-16 07:16:10 +0100

    Connect APIs in login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,9 +1,123 @@
-'use client';
-import Link from 'next/link';
-import { useState } from 'react';
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Notification from "../../components/global/Notification.js";
+import { useSendDataMutation } from "../../store/api/api.js";
+import { useSelector, useDispatch } from "react-redux";
+import { setIsAuth } from "../../store/slices/isAuthSlice";
+import { getDataFromLocalStorage } from "../../utils/localStorage";
+import { useEffect } from "react";
 
 export default function Login() {
-  const [electClassName, setElectClassName] = useState('elect selected');
+  const [electClassName, setElectClassName] = useState("elect selected");
+  const router = useRouter();
+  const [notification, setNotification] = useState({
+    message: "",
+    status: "",
+    show: false,
+  });
+  const [userData, setUserData] = useState({ email: "", password: "" });
+  const [userAuth, { isLoading, reset }] = useSendDataMutation();
+  const dispatch = useDispatch();
+
+  //CHECK IF VALUES ARE EITHER EMPTY, NULL OR UNDEFINED
+  function areValuesEmpty(obj) {
+    return Object.values(obj).some(
+      (value) => value === "" || value === null || value === undefined
+    );
+  }
+  //VALIDATE INPUTS
+  function validateRegistration(email, password) {
+    // Email validation using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isEmailValid = emailRegex.test(email);
+
+    // Password validation
+    const isPasswordValid = password.length >= 6;
+
+    if (isEmailValid === false) {
+      setNotification({
+        message: "Email is invalid",
+        status: "error",
+        show: true,
+      });
+      return;
+    } else if (isPasswordValid === false) {
+      setNotification({
+        message: "Password must have at least 7 characters",
+        status: "error",
+        show: true,
+      });
+      return;
+    }
+    return true;
+  }
+
+  //LOGIN FUNCTION
+  async function login(e) {
+    e.preventDefault();
+    const isDataEmpty = areValuesEmpty(userData);
+    if (isDataEmpty) {
+      setNotification({
+        message: "Empty Fields",
+        status: "error",
+        show: true,
+      });
+      return;
+    }
+    const validationResult = validateRegistration(
+      userData.email,
+      userData.password
+    );
+    if (validationResult) {
+      const request = await userAuth({
+        url: "auth/login",
+        data: {
+          email: userData.email,
+          password: userData.password,
+        },
+        type: "POST",
+      });
+      if (request?.data) {
+        const { data, message, success, accessToken, refreshToken, user } =
+          request?.data;
+        setNotification({
+          message: message,
+          status: "success",
+          show: true,
+        });
+        dispatch(
+          setIsAuth({
+            isAuth: true,
+            accessToken: accessToken,
+            refreshToken: refreshToken,
+            user: user,
+          })
+        );
+        user.role === "Elector"
+          ? router.push(`/dashboard/elector`)
+          : router.push(`/dashboard/organization`);
+      } else {
+        setNotification({
+          message: request?.error?.data?.error,
+          status: "error",
+          show: true,
+        });
+      }
+    }
+  }
+
+  const isAuthenticated = useSelector((state) => state.isAuth.isAuth);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const role = getDataFromLocalStorage("role");
+      role === "Elector"
+        ? router.push(`/dashboard/elector`)
+        : router.push(`/dashboard/organization`);
+    }
+  }, [isAuthenticated]);
 
   return (
     <main>
@@ -13,14 +127,57 @@ export default function Login() {
         </Link>
       </section>
 
+      {/* DISPLAY NOTIFICATION TO USER IF IT EXISTS */}
+      {notification.show ? (
+        <Notification
+          status={notification.status}
+          message={notification.message}
+          switchShowOff={() => {
+            setNotification((prev) => {
+              return { ...prev, show: false };
+            });
+          }}
+        />
+      ) : (
+        ""
+      )}
+
       <section className="signup-page-body">
         <div className={electClassName}>
           <h1>Login to Vota</h1>
           <form className="forms">
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button onClick={() => {}} className="btn">
-              Login to my account
+            <input
+              type="email"
+              placeholder="Email"
+              value={userData.email}
+              required
+              onChange={(e) => {
+                setUserData((prev) => {
+                  return { ...prev, email: e.target.value };
+                });
+              }}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={userData.password}
+              required
+              onChange={(e) => {
+                setUserData((prev) => {
+                  return { ...prev, password: e.target.value };
+                });
+              }}
+            />
+            <button
+              onClick={login}
+              className="btn"
+              disabled={isLoading ? true : false}
+            >
+              {isLoading ? (
+                <i className="bx bx-loader-alt bx-spin"></i>
+              ) : (
+                " Login to my account"
+              )}
             </button>
           </form>
           <p>
@@ -29,7 +186,7 @@ export default function Login() {
             </Link>
           </p>
           <p>
-            Already have an account?{' '}
+            Already have an account?{" "}
             <Link className="links" href="/signup">
               Sign Up
             </Link>
--- store/slices/isAuthSlice.js
+++ store/slices/isAuthSlice.js
@@ -2,13 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 import {
   getDataFromLocalStorage,
   saveDataToLocalStorage,
+  deleteDataFromLocalStorage,
 } from "../../utils/localStorage";
 
 const initialState = {
   isAuth: checkAuthStatus(),
   accessToken: null,
   refreshToken: null,
+  id: "",
+  fullName: "",
   email: "",
+  role: "",
 };
 
 // Function to check authentication status based on the presence of access and refresh tokens
@@ -23,21 +27,41 @@ export const isAuthSlice = createSlice({
   initialState,
   reducers: {
     setIsAuth: (state, action) => {
-      const { isAuth, accessToken, refreshToken } = action.payload;
-      console.log(state.isAuth);
-      // state.isAuth = isAuth;
+      const { isAuth, accessToken, refreshToken, user } = action.payload;
+      state.isAuth = isAuth;
       state.accessToken = accessToken;
       state.refreshToken = refreshToken;
+      state.id = user.id;
+      state.email = user.email;
+      state.fullName = user.fullName ? user.fullName : user.companyName;
+      state.role = user.role;
       // Save data in local storage if it exists
-      if (accessToken !== undefined && accessToken !== undefined) {
-        state.isAuth = true;
-        console.log(state.isAuth);
+      if (
+        accessToken !== undefined &&
+        accessToken !== undefined &&
+        user !== undefined
+      ) {
+        saveDataToLocalStorage("accessToken", accessToken);
+        saveDataToLocalStorage("refreshToken", refreshToken);
+        saveDataToLocalStorage("id", user.id);
+        saveDataToLocalStorage("email", user.email);
+        saveDataToLocalStorage(
+          "fullName",
+          user.fullName ? user.fullName : user.companyName
+        );
+        saveDataToLocalStorage("role", user.role);
       }
     },
     logOut: (state, action) => {
       state.isAuth = false;
       state.accessToken = null;
       state.refreshToken = null;
+      deleteDataFromLocalStorage("accessToken");
+      deleteDataFromLocalStorage("refreshToken");
+      deleteDataFromLocalStorage("id");
+      deleteDataFromLocalStorage("email");
+      deleteDataFromLocalStorage("fullName");
+      deleteDataFromLocalStorage("role");
     },
   },
 });
